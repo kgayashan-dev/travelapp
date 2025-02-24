a33db1751a671b8b48ff010cb84ba7c73ff2133e
gayashan-dev <mgayashan83@gmail.com>
2025-02-25 01:17:03 +0530

    uodate

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -5,7 +5,7 @@ import Button from "./Button";
 import { NAV_LINKS } from "@/constants";
 const Navbar = () => {
   return (
-    <nav className="flexBetween max-container padding-container py-5">
+    <nav className="flexBetween max-container padding-container py-5 text-gray-50">
       <Link href={"/"}>
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
